633ddb312a8f6f4a7625111fc2f183cf027428cc
Juan Benjamin <jubenjam@unix2.csc.calpoly.edu>
2019-04-03 10:50:33 -0700

    Initial Commit

--- planets.py
+++ planets.py
@@ -3,6 +3,7 @@ def weight_on_planets():
    mars = weight * 0.38
    jup = weight * 2.34
    print("\nOn Mars you would weigh {0} pounds.\nOn Jupiter you would weigh {1} pounds.".format(mars, jup))
+
    
    
 if __name__ == '__main__':
